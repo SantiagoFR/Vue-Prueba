77bdffeca06fba44d78b607bfd56e8afdd637890
Santiago <sfr0211@gmail.com>
2020-01-29 14:51:52 +0000

    diseño añadido

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,16 @@
   <div id="app">
     <b-row>
       <b-col v-for="(photo,i) in photos" :key="i" xl="2" lg="3" md="6" class="mt-5">
-        <b-card :title="'Image '+photo.id" :img-src="photo.url" @click="deleteItem(i)"></b-card>
+        <b-card :title="'Imagen '+photo.id" :img-src="photo.url" >
+          <b-button pill size="sm" href="#"  v-b-toggle="'accordion-' + photo.id" variant="outline-info">Mostrar/ocultar descripción</b-button>
+          <b-collapse :id="'accordion-'+photo.id">
+            <br>
+            <b-card-text>{{photo.title}}</b-card-text>
+          </b-collapse>          
+          <template v-slot:footer>
+            <b-button pill size="sm" variant="outline-danger" @click="deleteItem(i,photo.id)">Eliminar imagen</b-button>
+          </template>          
+        </b-card>
       </b-col>
     </b-row>
   </div>
@@ -23,11 +32,23 @@ export default {
       this.$http.get(baseURI)
       .then((result) => {
         console.log(result)
+        result.data.splice(0,4900)
+        console.log(result.data.length)
         this.photos = result.data
       })
     },
-    deleteItem: function(key){
+    deleteItem: function(key,id){
       this.photos.splice(key,1)
+      this.toastCount++
+      this.$bvToast.toast(`La imagen ${id} ha sido eliminada`, {
+        title: 'Imagen eliminada',
+        variant: 'success',
+        autoHideDelay: 5000,
+        appendToast: true
+      })
+    },
+    makeToast(append = false) {
+      
     }
   },
   created:function(){
--- src/main.js
+++ src/main.js
@@ -7,7 +7,9 @@ import bootstrap from 'bootstrap'
 import bootstrap_vue from 'bootstrap-vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
+import PortalVue from 'portal-vue'
 
+Vue.use(PortalVue)
 Vue.use(bootstrap_vue)
 
 Vue.prototype.$http = axios
